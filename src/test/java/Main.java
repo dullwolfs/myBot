75a0ffd815ab38e106c9071db51b4c467ba21e9e
dullwolfs <1091569752@qq.com>
2017-07-21 17:20:55 +0800

    上传代码

---
+++ src/test/java/Main.java
@@ -0,0 +1,16 @@
+import com.skill.bot.smartqq.Receiver;
+import org.junit.Test;
+
+/**
+ * Created by dullwolf on 2017/7/19.
+ */
+public class Main {
+
+
+    @Test
+    public void start() throws Exception{
+        Receiver.startClient();
+
+    }
+
+}
